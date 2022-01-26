7a58daf11fa83772ebae9f8250bf84b6d5d1008a
Oderystella <ddott59@gmail.com>
2022-01-26 15:14:53 +0900

    feat: get room list, create room using @api_view

--- rooms/urls.py
+++ rooms/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "rooms"
 
 urlpatterns = [
-    path("list/", views.list_rooms),
+    path("list/", views.rooms_view),
+    path("<int:pk>/", views.SeeRoomView.as_view()),
 ]
