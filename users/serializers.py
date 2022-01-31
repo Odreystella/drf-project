38062f035a3a33f62650bff946142fcee408fda6
Oderystella <ddott59@gmail.com>
2022-01-31 17:50:28 +0900

    feat: create MeView, user_detail
    - get user infomation
    - create ReadUserSerializer, WriteUserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -3,7 +3,40 @@ from rest_framework import serializers
 from .models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class RelatedUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("pk", "username", "first_name", "last_name", "email", "avatar", "superhost")
+        fields = (
+            "username", 
+            "first_name", 
+            "last_name", 
+            "email", 
+            "avatar", 
+            "superhost"
+            )
+
+
+class ReadUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "id", 
+            "username", 
+            "first_name", 
+            "last_name", 
+            "email", 
+            "avatar", 
+            "superhost",
+            "favs"
+            )
+
+
+class WriteUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "username", 
+            "first_name", 
+            "last_name", 
+            "email"
+        )
