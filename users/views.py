5b5bfb79800c13d22b5b57367adff3a1379bbf30
Oderystella <ddott59@gmail.com>
2022-02-15 01:23:08 +0900

    feat: create login function and issue jwt
    - 로그인 성공시 jwt 발급하기
    - 유저 인증은 장고의 authenticate 함수 사용

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
+import jwt
+
+from django.contrib.auth import authenticate
+from django.conf import settings
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
@@ -80,3 +84,20 @@ def user_detail(request, pk):
         return Response(UserSerializer(user).data)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(["POST"])
+def login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+
+    if not username or not password:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        encoded_jwt = jwt.encode({"user_id": user.pk}, settings.SECRET_KEY, algorithm="HS256")
+        return Response(data={"token": encoded_jwt})
+    else:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
