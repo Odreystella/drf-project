e2daaee5baa3e3d649ecccf23c67bbf96fb38b35
Oderystella <ddott59@gmail.com>
2022-03-02 23:56:23 +0900

    feat: create dynamic field using SerializerMethodField
    - send context in RoomsView to RoomSerializer
    - use method_name argument in SerializerMethodField

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
-
 from users.serializers import RelatedUserSerializer
+
 from .models import Room
 
 
 class RoomSerializer(serializers.ModelSerializer):
     user = RelatedUserSerializer()
+    is_fav = serializers.SerializerMethodField(method_name="get_your_fav_list")
 
     class Meta:
         model = Room
@@ -23,3 +24,11 @@ class RoomSerializer(serializers.ModelSerializer):
         if check_in == check_out:
             raise serializers.ValidationError("Not enouth time between times")
         return data
+
+    def get_your_fav_list(self, obj):
+        request = self.context.get("request")
+        if request:
+            user = request.user
+            if user.is_authenticated:
+                return obj in user.favs.all()
+        return False
