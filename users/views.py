46338fa7361bbe864a78030893e8723cbdc4b60d
Oderystella <ddott59@gmail.com>
2022-03-21 00:58:02 +0900

    feat: Add UserViewSet and urls

--- users/views.py
+++ users/views.py
@@ -3,47 +3,51 @@ import jwt
 from django.conf import settings
 from django.contrib.auth import authenticate
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser, AllowAny
 
 from rooms.models import Room
 from rooms.serializers import RoomSerializer
 from .models import User
 from .serializers import UserSerializer
+from .permissions import IsMe
 
 
-class UsersView(APIView):
-    """
-    유저 생성하기
-    """
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            new_user = serializer.save()
-            return Response(UserSerializer(new_user).data)
+class UsersViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.action == "list":
+            permission_classes = [IsAdminUser]
+        elif self.action == "create" or self.action =="retrieve":
+            permission_classes = [AllowAny]
         else:
-            return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+            permission_classes = [IsMe]
+        return [permission() for permission in permission_classes]
 
-
-class MeView(APIView):
-    """
-    로그인한 유저 정보 가져오기
-    로그인한 유저 정보 수정하기
-    """
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        return Response(UserSerializer(request.user).data)
-
-    def put(self, request):
-        serializer = UserSerializer(request.user, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+    @action(detail=False, methods=["POST"])
+    def login(self, request):
+        """
+        로그인 (성공 시 jwt 발급)
+        """
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            encoded_jwt = jwt.encode(
+                {"pk": user.pk}, settings.SECRET_KEY, algorithm="HS256"
+            )
+            return Response(data={"token": encoded_jwt, "id": user.pk})
         else:
-            return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 
 class FavsView(APIView):
@@ -70,34 +74,3 @@ class FavsView(APIView):
             except Room.DoesNotExist:
                 pass
         return Response(status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(["GET"])
-def user_detail(request, pk):
-    """
-    유저 정보 가져오기
-    """
-    try:
-        user = User.objects.get(pk=pk)
-        return Response(UserSerializer(user).data)
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-
-@api_view(["POST"])
-def login(request):
-    """
-    로그인 (성공 시 jwt 발급)
-    """
-    username = request.data.get("username")
-    password = request.data.get("password")
-    if not username or not password:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        encoded_jwt = jwt.encode(
-            {"pk": user.pk}, settings.SECRET_KEY, algorithm="HS256"
-        )
-        return Response(data={"token": encoded_jwt})
-    else:
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
